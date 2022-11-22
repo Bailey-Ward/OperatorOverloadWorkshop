214760474cb6c0bd87ddd055ed7dc86258c11454
Bailey Ward <25754670@students.lincoln.ac.uk>
2022-11-22 16:21:40 +0000

    nearly finished

--- OperatorOverloadWorkshop/OperatorOverloadWorkshop.cpp
+++ OperatorOverloadWorkshop/OperatorOverloadWorkshop.cpp
@@ -12,17 +12,20 @@ public:
         return value; //Getters and setters are used to access private variable
     }
     int value{};
+
     A(int v) : value(v) {}
     void print() { std::cout << "My value is " << value << std::endl; }
 
+    A operator++(int){  // ++ operator overloaded to increment any A type objects
+        int temp = value + 1;
+        return temp;
+    }
+    A operator+(A) {  //+ operator overloaded to add any A type objects
+        int added = value + value;
+        return added;
+    }
 };
 
-/*A operator+(const A&) {
-}
-
-A operator++(const A&){
-}
-*/
 
 
 A add(A a1, A a2)
@@ -33,6 +36,7 @@ A add(A a1, A a2)
 
 int main()
 {
+ 
     A a1 = A(2);
     A a2 = A(3);
 
@@ -42,7 +46,11 @@ int main()
     A a3 = add(a1, a2);
     a3.print();
 
-    /*A a4 = ++a3;
+    A a4 = a1 + a2 +a3;
     a4.print();
-    return 0;*/
+
+    A a5 = a4++;
+    a5.print();
+    return 0;
+
 }
